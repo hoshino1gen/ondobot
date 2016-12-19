8cb027dac53c897e10f7e09633d07d91b39c81ee
tk <ro99881d@gmail.com>
2016-12-19 11:02:31 +0900

    be

--- push.php
+++ push.php
@@ -9,12 +9,28 @@ echo "xxx";
 
 //DATABASE_URL = postgres://cbpwfjdfgminnm:8b610ee14ab82f5af2f0ee50932155a08744a2854c34b488469316cde84ff28c@ec2-54-235-90-107.compute-1.amazonaws.com:5432/do7eaa2rdtvvc
 
-$url = parse_url(getenv('DATABASE_URL'));
+try {
+  $url = parse_url(getenv('DATABASE_URL'));
+  $dsn = sprintf('pgsql:host=%s;dbname=%s', $url['host'], substr($url['path'], 1));
+
+  $pdo = new PDO($dsn, $url['user'], $url['pass']);
+  var_dump($pdo->getAttribute(PDO::ATTR_SERVER_VERSION));
+
+  //insert into sample (crtuser,) values( );
+  $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+  $stmt = $db->query("select * from sample");
+  $users = $stmt->fetchAll(PDO::FETCH_ASSOC);
+  foreach ( $users as $user ) {
+    var_dump($user);
+  }
+
+  echo $stmt->rowCount();
 
 
-$dsn = sprintf('pgsql:host=%s;dbname=%s', $url['host'], substr($url['path'], 1));
+} catch (PDOException $e ) {
+  echo $e->getMessage();
+}
 
-$pdo = new PDO($dsn, $url['user'], $url['pass']);
-var_dump($pdo->getAttribute(PDO::ATTR_SERVER_VERSION));
 
 ?>
