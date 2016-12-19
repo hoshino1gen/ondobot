19189bf76d96001b36867a5bc6b7ed4db888af86
tk <ro99881d@gmail.com>
2016-12-19 10:31:14 +0900

    new

--- push.php
+++ push.php
@@ -7,5 +7,14 @@ date_default_timezone_set('Asia/Tokyo');
 
 echo "xxx";
 
+//DATABASE_URL = postgres://cbpwfjdfgminnm:8b610ee14ab82f5af2f0ee50932155a08744a2854c34b488469316cde84ff28c@ec2-54-235-90-107.compute-1.amazonaws.com:5432/do7eaa2rdtvvc
+
+$url = parse_url(getenv('DATABASE_URL'));
+
+
+$dsn = sprintf('pgsql:host=%s;dbname=%s', $url['host'], substr($url['path'], 1));
+
+$pdo = new PDO($dsn, $url['user'], $url['pass']);
+var_dump($pdo->getAttribute(PDO::ATTR_SERVER_VERSION));
 
 ?>
